d98d1b393bb6571e7c1d9f12bf05311179ee34a1
Kevin Phelan <tromboneist16@gmail.com>
2020-01-03 18:32:12 -0500

    uploading my first vue.js app

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="Header">
+        <h1>TodoList</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name:"Header"
+}
+</script>
+
+<style scoped>
+    .Header{
+        background: #333;
+        color: #fff;
+        text-align: center;
+        padding : 10px;
+    }
+    .Header a {
+        color:#fff;
+        padding-left: 5px;
+    }
+
+</style>
